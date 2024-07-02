f1dfa715d1d69e4cb9ab76f9a7ffaf730c6cca3f
AnaFidencio <fidenciosilvaanaclara@gmail.com>
2024-07-02 11:08:09 -0300

    Chamada da API. Continuação do tutorial pós greve

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qud9uc3peg"
+    "revision": "0.0vqqk0vdla8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
