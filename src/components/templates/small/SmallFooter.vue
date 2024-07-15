a0274b29ef657de2a9a9fcaee77a6af44561175b
AnaFidencio <fidenciosilvaanaclara@gmail.com>
2024-07-15 11:53:20 -0300

    Criando registro de autenticação

--- src/components/templates/small/SmallFooter.vue
+++ src/components/templates/small/SmallFooter.vue
@@ -1,12 +1,22 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { onBeforeRouteUpdate } from 'vue-router';
+
+const showMenu = ref(false);
+
+onBeforeRouteUpdate(() => {
+  showMenu.value = false;
+});
+</script>
+
 <template>
-    <div id="footerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
+  <div id="footerMenu" :style="{ display: showMenu ? 'block' : 'none' }">
     <RouterLink to="/">
-      <i class="mdi mdi-home-outiline" />
+      <i class="mdi mdi-home-outline" />
       Home
     </RouterLink>
     <RouterLink to="/login">
-      <Account size="25" fillColor="#282828" />
+      <i class="mdi mdi-account" />
       Login
     </RouterLink>
   </div>
@@ -23,8 +33,8 @@
       <i class="mdi mdi-cart-outline" />
       Carrinho
     </RouterLink>
-    
-      <div class="hamburguer" @click="showMenu = !showMenu">
+
+    <div class="hamburger" @click="showMenu = !showMenu">
       <i class="mdi mdi-menu" />
       Menu
     </div>
@@ -32,43 +42,48 @@
 </template>
 <style scoped>
 #footerMenu {
-  position: fixed;
-  bottom: 15%;
-  right: 0;
+    position: fixed;
+    bottom: 15%;
+    right: 0;
 
-  width: 20%;
-  border-top: #eeeeee 1px solid;
-  background-color: white;
+    width: 20%;
+    border-top: #EEEEEE 1px solid;
+    background-color: white;
 
-  display: block;
-  padding: 1rem;
+    display: block;
+    padding: 1rem;
 }
 
 #footerMenu a {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  text-decoration: none;
-  color: #282828;
-  font-size: 1rem;
-  transition: color 0.3s;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    text-decoration: none;
+    color: #282828;
+    font-size: 1rem;
+    transition: color 0.3s;
 }
+
 
 .icons {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 1.7rem;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
 }
 
-.icons a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-decoration: none;
-  color: #282828;
-  font-size: 1rem;
-  transition: color 0.3s;
+.icons a,
+.icons .hamburger {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
+    color: #282828;
+    font-size: 1rem;
+    transition: color 0.3s;
+}
+
+.hamburger:hover {
+    cursor: pointer;
 }
 </style>
